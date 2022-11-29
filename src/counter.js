6b5f9333a15a922ecb331c812adbe0f2eb5bf3c8
Elinoa <elinoa.resnick17@gmail.com>
2022-11-29 15:27:36 +0200

    Adeed input

--- src/counter.js
+++ src/counter.js
@@ -4,27 +4,32 @@ import { useState } from "react";
 
 function Counter() {
     const[count, setCount] = useState(1)
+    const [delta, setDelta] = useState(1)
     function incr(){
         setCount(
             function(oldCount){
-                return oldCount +1
+                return oldCount +delta
             }
         )
         console.log(count)
     }
+    function handleDelta(e){
+        console.log(e);
+        setDelta(Number(e.target.value))
+    }
     function reset(){
-        setCount(
-            function(oldCount){
-                return oldCount =1
-            }
+        setCount(0
+            // function(oldCount){
+            //     return oldCount =1
+            // }
         )
-        console.log(count)
     }
   return (
     <div>
-        <h1 class="header">Counter</h1>
+        <h1 className="header">Counter</h1>
         <p>Counter is at {count}</p>
-        <button onClick={incr}>Click to add 1 to counter</button>
+        <p><input type= "number" value = {delta} onChange = {handleDelta}/></p>
+        <button onClick={incr}>Click to add {delta} to counter</button>
         <p><button onClick={reset}>Click to reset counter</button></p>
     </div>
   );
