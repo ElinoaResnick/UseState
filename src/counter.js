efb3b6a3e5de03ad5c95c6136eed1e4abbedc47b
Elinoa <elinoa.resnick17@gmail.com>
2022-11-29 15:45:44 +0200

    Added prps

--- src/counter.js
+++ src/counter.js
@@ -2,9 +2,10 @@ import './Counter.css';
 
 import { useState } from "react";
 
-function Counter() {
+function Counter(props) {
+    const {delta} = props
     const[count, setCount] = useState(1)
-    const [delta, setDelta] = useState(1)
+
     function incr(){
         setCount(
             function(oldCount){
@@ -13,10 +14,7 @@ function Counter() {
         )
         console.log(count)
     }
-    function handleDelta(e){
-        console.log(e);
-        setDelta(Number(e.target.value))
-    }
+
     function reset(){
         setCount(0
             // function(oldCount){
@@ -28,7 +26,7 @@ function Counter() {
     <div>
         <h1 className="header">Counter</h1>
         <p>Counter is at {count}</p>
-        <p><input type= "number" value = {delta} onChange = {handleDelta}/></p>
+
         <button onClick={incr}>Click to add {delta} to counter</button>
         <p><button onClick={reset}>Click to reset counter</button></p>
     </div>
