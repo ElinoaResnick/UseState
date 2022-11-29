cc7070e2d49c8bb070d1fd7424ba2d1edbc95652
Elinoa <elinoa.resnick17@gmail.com>
2022-11-29 15:45:24 +0200

    Added props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 
 import './App.css';
 import Counter from './counter';
+import { useState } from "react";
+
 
 function App() {
+  const [delta, setDelta] = useState(1)
+
+  function handleDelta(e){
+  console.log(e);
+  setDelta(Number(e.target.value))
+  }
+
   return (
     <div className="App">
-      <Counter/>
+      <p><input type= "number" value = {delta} onChange = {handleDelta}/></p>
+      <Counter delta={delta}/>
+      <Counter delta={delta}/>
     </div>
   );
 }
